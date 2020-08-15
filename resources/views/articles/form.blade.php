4e31862a298eedce453236725601eed7f5933068
yumauma20 <71yuma10@gmail.com>
2020-08-15 23:10:11 +0900

    form.blade.phpの見直し

--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -1,9 +1,9 @@
 @csrf
 <div class="md-form">
   <label>タイトル</label>
-  <input type="text" name="title" class="form-control" required value="{{ old('title') ?? $article->title }}">
+  <input type="text" name="title" class="form-control" required value="{{ old('title') ?? $article->title ?? ''}}">
 </div>
 <div class="form-group">
   <label></label>
-  <textarea name="body" required class="form-control" rows="16" placeholder="本文">{{ old('body') ?? $article->body }}</textarea>
+  <textarea name="body" required class="form-control" rows="16" placeholder="本文">{{ old('body') ?? $article->body ?? '' }}</textarea>
 </div>

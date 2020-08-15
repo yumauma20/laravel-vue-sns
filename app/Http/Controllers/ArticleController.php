98eae61ddacf1facbe4b1d02773ceea6549e07a7
yumauma20 <71yuma10@gmail.com>
2020-08-15 23:10:33 +0900

    削除機能の作成

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -39,4 +39,10 @@ class ArticleController extends Controller
     $article->fill($request->all())->save();
     return redirect()->route('articles.index');
   }
+
+  public function destroy(Article $article)
+  {
+    $article->delete();
+    return redirect()->route('articles.index');
+  }
 }

70a5c414ff38e885f79cfd9558ade3b97a481db8
yumauma20 <71yuma10@gmail.com>
2020-08-17 00:57:18 +0900

    クリックでいいねできるように機能追加

--- resources/js/components/ArticleLike.vue
+++ resources/js/components/ArticleLike.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <button type="button" class="btn m-0 p-1 shadow-none">
-      <i class="fas fa-heart mr-1" :class="{'red-text':this.isLikedBy}"/>
+      <i class="fas fa-heart mr-1" 
+        :class="{'red-text':this.isLikedBy}"
+        @click="clickLike"
+      />
     </button>
     {{ countLikes }}
   </div>
@@ -18,12 +21,43 @@
         type: Number,
         default: 0,
       },
+      authorized: {
+        type: Boolean,
+        default: false,
+      },
+      endpoint: {
+        type: String,
+      },
     },
     data() {
       return {
         isLikedBy: this.initialIsLikedBy,
         countLikes: this.initialCountLikes,
       }
+    },
+    methods: {
+      clickLike() {
+        if(!this.authorized) {
+          alert('ログインしてないとできないよ')
+          return
+        }
+        this.isLikedBy
+          ? this.unlike()
+          : this.like()
+      },
+
+      async like() {
+        const response = await axios.put(this.endpoint)
+
+        this.isLikedBy = true
+        this.countLikes = response.data.countLikes
+      },
+      async unlike() {
+        const response = await axios.delete(this.endpoint)
+
+        this.isLikedBy = false
+        this.countLikes = response.data.countLikes
+      },
     },
   }
 </script>

267dd66640a5de55ac54e1eede52fc2293e84779
yumauma20 <71yuma10@gmail.com>
2020-08-24 02:00:47 +0900

    記事投稿処理の編集

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Article;
+use App\Tag;
 use App\Http\Requests\ArticleRequest;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 
@@ -31,6 +33,12 @@ class ArticleController extends Controller
     $article->fill($request->all());
     $article->user_id = $request->user()->id;
     $article->save();
+
+    $request->tags->each(function ($tagName) use ($article) {
+      $tag = Tag::firstOrCreate(['name' => $tagName]);
+      $article->tags()->attach($tag);
+    });
+
     return redirect()->route('articles.index');
   }
 

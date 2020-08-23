c0e8db16de999f2797b7ce980756367677d81161
yumauma20 <71yuma10@gmail.com>
2020-08-24 00:28:53 +0900

    vue-tags-inputをインストール

--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -4,6 +4,9 @@
   <input type="text" name="title" class="form-control" required value="{{ old('title') ?? $article->title ?? ''}}">
 </div>
 <div class="form-group">
+  <article-tags-input></article-tags-input>
+</div>
+<div class="form-group">
   <label></label>
   <textarea name="body" required class="form-control" rows="16" placeholder="本文">{{ old('body') ?? $article->body ?? '' }}</textarea>
 </div>
